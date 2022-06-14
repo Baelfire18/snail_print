8526584dde300e94020c3d4cec823a698675ed14
Jose Antonio Castro <jacastro18@uc.cl>
2022-06-14 01:10:29 -0400

    fix(terminal size): fix windows bugs

--- snail_print/__init__.py
+++ snail_print/__init__.py
@@ -28,8 +28,11 @@ def snail_print(
     try:
         height, _ = get_terminal_size()
     except OSError as e:
-        if e.errno == 25:
+        if e.errno in (5, 25):
             height = 80
+        else:
+            raise e
+
     prev_output = stdout_handler.get_last_output_line().strip(RLL)
     string = sep.join([str(i) for i in objects])
     string_len = len(string)
