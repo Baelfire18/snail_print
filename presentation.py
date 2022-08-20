708d68edf86a7acd504a61099f5ce1c0b1971496
Jose Antonio Castro <jacastro18@uc.cl>
2022-08-20 11:18:38 -0400

    modify presentation

--- presentation.py
+++ presentation.py
@@ -1,20 +1,20 @@
 from snail_print import snail_print
-from color import bcolors
+from color import bcolors as b
 import time
 
-delay = 0.15
+delay = 0.05
 meta_delay = 0.3
-print(f"{bcolors.GREEN}❯{bcolors.NORMAL} python")
+print(f" {b.GREEN}❯{b.NORMAL} python")
 time.sleep(1)
 print(
-    """Python 3.10.4 (main, Apr  2 2022, 18:06:22) [GCC 9.4.0] on linux
-Type "help", "copyright", "credits" or "license" for more information"""
+    """ Python 3.10.4 (main, Apr  2 2022, 18:06:22) [GCC 9.4.0] on linux
+ Type "help", "copyright", "credits" or "license" for more information"""
 )
 time.sleep(1)
-print(">>>", end=" ")
-snail_print("from snail_print import snail_print", delay=delay)
+print(f" {b.NORMAL}>>>", end=" ")
+snail_print(f"{b.GREEN}from{b.NORMAL} {b.ORANGE}snail_print{b.NORMAL} {b.GREEN}import{b.NORMAL} {b.NORMAL}snail_print", delay=delay)
 
-print(">>>", end=" ")
-snail_print(f'snail_print("Hello World", delay={meta_delay})', delay=delay)
-snail_print("Hello World", delay=meta_delay)
-print(">>>")
+print(f" {b.NORMAL}>>>", end=" ")
+snail_print(f'{b.NORMAL}snail_print({b.YELLOW}"Hello World"{b.NORMAL}, delay={b.GREEN}{meta_delay}{b.NORMAL})', delay=delay)
+snail_print(" Hello World", delay=meta_delay)
+print(" >>>")
